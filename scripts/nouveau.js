d9c52cb40ac15af5a2edfea660bb6d4a50d1227b
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-10-25 16:25:03 +0200

    Ajout message erreur lors du click sur Creer

--- scripts/nouveau.js
+++ scripts/nouveau.js
@@ -49,7 +49,42 @@ $( document ).ready(function() {
   $("#bouttonCreer").on('click',function(){
     var t2=$("#numsecu").val().match(/\d{13}/);
     if($("#nom").val() == ''||$("#prenom").val() == ''||$("#anniversaire").val()==''||$("#adresse").val()==''||t2==null){
-
+      if (t2==null){
+        $("#numsecu").addClass("is-invalid");
+      }else{
+        $("#numsecu").removeClass("is-invalid");
+        $("#numsecu").addClass("is-valid");
+      }
+      if ($("#adresse").val() == ''){
+        console.log("Entrée dans if");
+        $("#adresse").addClass("is-invalid");
+        $("#adresse").val('');
+      }else{
+        $("#adresse").removeClass("is-invalid");
+        $("#adresse").addClass("is-valid");
+      }
+      if ($("#anniversaire").val()==''){
+        $("#anniversaire").addClass("is-invalid");
+        $("#anniversaire").val('');
+      }else{
+        $("#anniversaire").removeClass("is-invalid");
+        $("#anniversaire").addClass("is-valid");
+      }
+      if ($("#prenom").val() == ''){
+        $("#prenom").addClass("is-invalid");
+        $("#prenom").val('');
+      }else{
+        $("#prenom").removeClass("is-invalid");
+        $("#prenom").addClass("is-valid");
+      }
+      if ($("#nom").val() == ''){
+        console.log("Entrée dans if");
+        $("#nom").addClass("is-invalid");
+        $("#nom").val('');
+      }else{
+        $("#nom").removeClass("is-invalid");
+        $("#nom").addClass("is-valid");
+      }
     }else{
       $("#labelnom").html($("#nom").val());
       $("#labelprenom").html($("#prenom").val());
